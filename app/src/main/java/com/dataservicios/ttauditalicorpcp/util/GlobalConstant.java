d2e079e2e46957dea9eb4c4013b5a6f66683a260
Jaime <jcdiaz356@hotmasil.com>
2018-08-10 13:53:12 -0500

    Initial commit

---
+++ app/src/main/java/com/dataservicios/ttauditalicorpcp/util/GlobalConstant.java
@@ -0,0 +1,19 @@
+package com.dataservicios.ttauditalicorpcp.util;
+/**
+ * Created by usuario on 11/11/2014.
+ */
+public final class GlobalConstant {
+
+//    public static String dominio = "http://ttaudit.com";
+    public static String dominio = "http://192.168.8.110";
+    public static final String URL_USER_IMAGES = dominio + "/media/users/";
+    public static final String URL_PUBLICITY_IMAGES = dominio + "/media/images/alicorp/publicities/";
+    public static String directory_images = "/Pictures/" ;
+    public static final String JPEG_FILE_PREFIX = "_cp_";
+    public static final String JPEG_FILE_SUFFIX = ".jpg";
+    public static final String ALBUN_NAME = "cp_Photo";
+    public static final String ALBUN_NAME_TEMP = "cp_Photo_Temp";
+    public static final String ALBUN_NAME_BACKUP = "cp_Photo_Backup";
+
+}
+
